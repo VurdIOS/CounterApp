761affa66b814904d9379d745c3119e7bb213b9a
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-07-14 16:18:40 +0300

    blue

--- CounterApp/Developer/DeveloperViewController.swift
+++ CounterApp/Developer/DeveloperViewController.swift
@@ -11,7 +11,7 @@ class DeveloperViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .blue
     }
     
 
